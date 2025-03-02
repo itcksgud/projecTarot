0b3720dc0ee94a7003f6b9a74878f64be34a4017
unknown <kingcrown20200@gmail.com>
2025-03-02 14:18:57 +0900

    cookies

--- middleware.js
+++ middleware.js
@@ -7,8 +7,8 @@ export function middleware(req) {
   if (sessionToken) {
     const res = NextResponse.next();
 
-    // 새로운 쿠키 설정 (SameSite=None으로 설정하여 인앱 브라우저에서도 유지)
-    res.cookies.set("token", sessionToken, {
+    // 쿠키를 올바른 문자열로 저장
+    res.cookies.set("token", String(sessionToken), {
       httpOnly: true,
       secure: true,
       sameSite: "None",
